93c96660521bff251087a5f01e0b7f22bb7a3535
PorcupineRain <rayne.jurchen@gmail.com>
2024-10-01 21:20:11 +0200

    add solution to first task of task 8

---
+++ 08-event-handling/script.js
@@ -0,0 +1,22 @@
+Vue.createApp({
+  data() {
+    return {
+      x: 12,
+      y: 4,
+      fruitBasket: [
+        "🍏 Apple",
+        "🍌 Banana",
+        "🍉 Melon",
+        "🫐 Blueberry",
+        "🍓 Strawberry",
+        "🍍 Ananas",
+        "🥭 Mango",
+      ],
+    };
+  },
+  methods: {
+    showPosition(event) {
+      (this.x = event.offsetX), (this.y = event.offsetY);
+    },
+  },
+}).mount("#app");
